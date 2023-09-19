056a64f7c1eae0ee9c2b07183fe90529f5fb1a1c
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-19 17:19:39 -0300

    ✬ feat: auth

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, UseGuards } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { BillingService } from './billing.service';
+import { JwtAuthGuard, RmqService } from '@app/cammon';
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  private readonly logger = new Logger(BillingController.name);
+
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.billingService.getHello();
+  }
+
+  @EventPattern('order_created')
+  @UseGuards(JwtAuthGuard)
+  async handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    this.billingService.bill(data);
+    this.rmqService.ack(context);
   }
 }
