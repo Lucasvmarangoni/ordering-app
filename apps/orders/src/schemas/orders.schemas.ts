fea80f53866477abef0c69a9d2a952d46413afc4
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-19 12:29:28 -0300

    ✬ feat:  add orders schema

--- apps/orders/src/schemas/orders.schemas.ts
+++ apps/orders/src/schemas/orders.schemas.ts
@@ -10,7 +10,7 @@ export class Order extends AbstractDocument {
   price: number;
 
   @Prop()
-  phoneNUmber: string;
+  phoneNumber: string;
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order);
