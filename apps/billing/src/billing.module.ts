eadf4b25934c82354b60820817bdc445c106a971
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-19 15:43:55 -0300

    ✬ feat: billing and rmq

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import { RmqModule } from '@app/cammon';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    RmqModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
